ac9dc030ffb6799f761ea0f337779ecda945bf44
Edvin Kornekho <notmy@email.com>
2020-09-10 13:42:03 +0300

    Pr2 Added

---
+++ src/ru/mirea/ikbo0819/pr2/Author.java
@@ -0,0 +1,45 @@
+package ru.mirea.ikbo0819.pr2;
+
+import java.lang.*;
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    Author(String name, String email, char gender){
+       this.name = name;
+       this.gender = gender;
+       this.email = email;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+      public String getName(){
+            return name;
+      }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public String toString() {
+        return  name + " (" + gender + ") at " + email;
+    }
+
+
+}
---
+++ src/ru/mirea/ikbo0819/pr2/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.ikbo0819.pr2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Edgar Allan Poe","@gothic_tribe.email.com",'m');
+        System.out.println(a1); // toString()
+    }
+}
